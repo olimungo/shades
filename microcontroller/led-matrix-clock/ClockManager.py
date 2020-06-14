c21e771adb795baf0f01c11d04fca4fadfe84d0b
Olivier Mungo <olivier@mungo.eu>
2020-06-14 23:08:56 +0200

    .

--- microcontroller/led-matrix-clock/ClockManager.py
+++ microcontroller/led-matrix-clock/ClockManager.py
@@ -4,7 +4,6 @@ from max7219 import Matrix8x8
 from gc import mem_free, collect
 from NtpTime import NtpTime
 from SmallClock import SmallClock
-from LargeClock import LargeClock
 import settings
 
 class Gpio:
@@ -14,9 +13,8 @@ class Gpio:
 class ClockManager:
     brightness = 0
 
-    def __init__(self, wifiManager, mqttManager, webServer):
+    def __init__(self, wifiManager, webServer):
         self.wifiManager = wifiManager
-        self.mqttManager = mqttManager
         self.webServer = webServer
 
         self.spi = SPI(1, baudrate=10000000, polarity=1, phase=0)
@@ -26,29 +24,11 @@ class ClockManager:
         self.board.show()
 
         self.time = NtpTime(self.wifiManager)
-
         self.smallClock = SmallClock(self.board, self.time)
         self.smallClock.start()
 
-        self.largeClock = None
-
         self.loop = get_event_loop()
-        # self.loop.create_task(self._checkMqtt())
         self.loop.create_task(self._pollWebServer())
-        # self.loop.create_task(self._sendState())
-        # self.loop.create_task(self._checkState())
-
-    async def _checkMqtt(self):
-        while True:
-            while self.mqttManager.isConnected():
-                collect()
-                self._checkForMqttMessage()
-                await sleep_ms(500)
-
-            while not self.mqttManager.isConnected():
-                await sleep(1)
-
-            self._sendMqttState()
 
     async def _pollWebServer(self):
         while True:
@@ -58,14 +38,12 @@ class ClockManager:
                 if not emptyRequest:
                     if path == "/" or path == "/index.html":
                         self._index(client)
-                    elif path == "/action/clock/small":
-                        self.loop.create_task(self._setClock(client, "SMALL"))
-                    elif path == "/action/clock/large":
-                        self.loop.create_task(self._setClock(client, "LARGE"))
                     elif path == "/action/brightness/more":
                         self._setBrightness(client, 1)
                     elif path == "/action/brightness/less":
                         self._setBrightness(client, -1)
+                    elif path == "/settings/set-net":
+                        self._setNet(client, queryStringsArray)
                     elif path == "/settings/connect":
                         self._connect(client, queryStringsArray)
                     elif path == "/favicon.ico":
@@ -79,31 +57,6 @@ class ClockManager:
 
             await sleep_ms(250)
 
-    def _checkForMqttMessage(self):
-        try:
-            message = self.mqttManager.checkMessage()
-
-            if not message == None:
-                pass
-        except Exception as e:
-            print("> ClockManager._checkForMqttMessage exception: {}".format(e))
-
-    async def _sendState(self):
-        while True:
-            self._sendMqttState()
-
-            await sleep(30)
-
-    def _sendMqttState(self):
-        try:
-            group = settings.readGroup()
-
-            state = "{" + '"group": "{}", "state": "{}"'.format(group, "TO DO") + "}"
-
-            self.mqttManager.publishState(state)
-        except Exception as e:
-            print("> ClockManager._sendMqttState exception: {}".format(e))
-
     def _index(self, client):
         ip = self.wifiManager.getIp()
         netId, essid, group = settings.readSettings()
@@ -116,36 +69,6 @@ class ClockManager:
         }
 
         self.webServer.index(client, interpolate)
-
-    async def _setClock(self, client, type):
-        if type == "SMALL":
-            if self.largeClock != None:
-                self.largeClock.stop()
-                self.largeClock = None
-
-            self.board.fill(0)
-            self.board.show()
-            await sleep_ms(250)
-            
-            if self.smallClock == None:
-                collect()
-                self.smallClock = SmallClock(self.board, self.time)
-                self.smallClock.start()
-        else:
-            if self.smallClock != None:
-                self.smallClock.stop()
-                self.smallClock = None
-
-            self.board.fill(0)
-            self.board.show()
-            await sleep_ms(250)
-
-            if self.largeClock == None:
-                collect()
-                self.largeClock = LargeClock(self.board, self.time)
-                self.largeClock.start()
-
-        self.webServer.ok(client)
 
     def _setBrightness(self, client, increment):
         if self.brightness + increment > 0 and self.brightness + increment < 10:
@@ -177,29 +100,4 @@ class ClockManager:
                 self.wifiManager.connect(essid, password)
 
         self.webServer.ok(client)
-
-    async def _checkState(self):
-        while True:
-            # if self.task == None:
-            #     self.board.fill(0)
-            #     self.board.show()
-
-            # if self.state == SignState.SHOW:
-            #     if self.task == None:
-            #         self.task = self._stateShow()
-            #         self.loop.create_task(self.task)
-            # elif self.state == SignState.HOTEL:
-            #     if self.task == None:
-            #         self.task = self._stateHotel()
-            #         self.loop.create_task(self.task)
-            # elif self.state == SignState.ON:
-            #     if self.task == None:
-            #         self.task = self._stateOnOff(1)
-            #         self.loop.create_task(self.task)
-            # elif self.state == SignState.OFF:
-            #     if self.task == None:
-            #         self.task = self._stateOnOff(0)
-            #         self.loop.create_task(self.task)
-
-            await sleep_ms(500)
 
--- microcontroller/led-matrix-clock/main.py
+++ microcontroller/led-matrix-clock/main.py
@@ -1,25 +1,19 @@
 from uasyncio import get_event_loop
-# from mDnsServer import mDnsServer
-# from MqttManager import MqttManager
+from mDnsServer import mDnsServer
 from WifiManager import WifiManager
 from WebServer import WebServer
 from ClockManager import ClockManager
 from settings import readNetId
 
 PUBLIC_NAME = "clock"
-MQTT_BROKER_NAME = "nestor.local"
-MQTT_BROKER_TOPIC_NAME = "clock"
 
 netId = readNetId()
 publicName = "{}-{}".format(PUBLIC_NAME, netId)
 
 wifiManager = WifiManager(publicName)
-# mdnsServer = mDnsServer(wifiManager, publicName)
-# mqttManager = MqttManager(mdnsServer, MQTT_BROKER_NAME, netId, MQTT_BROKER_TOPIC_NAME)
+mdnsServer = mDnsServer(wifiManager, publicName)
 webServer = WebServer(wifiManager)
-
-# clockManager = ClockManager(wifiManager, mqttManager, webServer)
-clockManager = ClockManager(wifiManager, None, webServer)
+clockManager = ClockManager(wifiManager, webServer)
 
 loop = get_event_loop()
 loop.run_forever()
